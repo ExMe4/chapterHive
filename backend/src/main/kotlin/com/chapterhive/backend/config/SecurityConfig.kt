b026e8bf89e22aba07aa91464bdb0bc187297279
ExMe4 <joao.f.regateiro@gmail.com>
2025-01-28 13:48:51 +0100

    added spring security configuration

---
+++ backend/src/main/kotlin/com/chapterhive/backend/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.chapterhive.backend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth
+                    // .requestMatchers("/api/reviews/**").permitAll()
+                    .anyRequest().permitAll() // permit all requests for now
+            }
+        return http.build()
+    }
+}
