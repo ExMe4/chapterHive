321cb043483793ac123490e4da1328d0e3d23833
ExMe4 <joao.f.regateiro@gmail.com>
2025-02-03 13:02:32 +0100

    [backend] added Google OAuth2 Configuration for login

--- backend/src/main/kotlin/com/chapterhive/backend/config/SecurityConfig.kt
+++ backend/src/main/kotlin/com/chapterhive/backend/config/SecurityConfig.kt
@@ -3,6 +3,8 @@ package com.chapterhive.backend.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -16,6 +18,15 @@ class SecurityConfig {
                 auth
                     // .requestMatchers("/api/reviews/**").permitAll()
                     .anyRequest().permitAll() // permit all requests for now
+            }
+            .oauth2Login { oauth2 ->
+                oauth2
+                    .userInfoEndpoint { userInfo ->
+                        userInfo
+                            .oidcUserService(OidcUserService())  // For Google Web
+                            .userService(DefaultOAuth2UserService())  // For Google Android/iOS
+                    }
+                    .loginPage("/login")
             }
         return http.build()
     }
