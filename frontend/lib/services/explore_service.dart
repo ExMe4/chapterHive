84a380ab845398bebb1d2e8e6e7d93a9223d9627
ExMe4 <joao.f.regateiro@gmail.com>
2025-03-21 12:24:09 +0100

    [frontend] added explore service

---
+++ frontend/lib/services/explore_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ExploreService {
+  final String apiUrl = "http://10.0.2.2:8080/api/explore"; // TODO change after
+
+  Future<Map<String, dynamic>?> search(String query) async {
+    try {
+      final response = await http.get(Uri.parse("$apiUrl?query=$query"));
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        print("Failed to load search results: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      print("Error fetching search results: $e");
+      return null;
+    }
+  }
+}
